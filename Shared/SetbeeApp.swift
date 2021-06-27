31ff55bb0b4db3a2568afa5eac2aad861e8e06e1
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-06-27 15:08:36 +0200

    Initial Commit

---
+++ Shared/SetbeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetbeeApp.swift
+//  Shared
+//
+//  Created by Kamil Powałowski on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SetbeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
