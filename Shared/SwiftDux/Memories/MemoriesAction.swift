9346ab9fb9b77e748c38ea107c433204c92185db
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-07-12 21:00:55 +0200

    Fixed errors raised by Code Climate

--- Shared/SwiftDux/Memories/MemoriesAction.swift
+++ Shared/SwiftDux/Memories/MemoriesAction.swift
@@ -2,6 +2,22 @@
 //  Created by Kamil Powałowski on 27/06/2021.
 //
 
+import Combine
+import ParseSwift
 import SwiftDux
 
-enum MemoriesAction: Action {}
+enum MemoriesAction: Action {
+    case set([Memory])
+}
+
+extension MemoriesAction {
+    static func getAll() -> ActionPlan<AppState> {
+        ActionPlan<AppState> { _ -> AnyPublisher<Action, Never> in
+            let query = Memory.query()
+            return query.findAllPublisher()
+                .map { MemoriesAction.set($0) }
+                .catch { Just(MessageAction.set(.error($0.message))) }
+                .eraseToAnyPublisher()
+        }
+    }
+}
