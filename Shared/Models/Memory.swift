16091861bbe1c1547f54b83b56622b2081111cac
Kamil Powałowski <kamil.powalowski@gmail.com>
2021-09-29 20:42:27 +0200

    Improved search functionality by applying search to separated tags

--- Shared/Models/Memory.swift
+++ Shared/Models/Memory.swift
@@ -59,13 +59,16 @@ struct Memory: Identifiable, Codable, Equatable {
 
 extension Memory: Fuseable {
     var properties: [FuseProperty] {
-        [
+        let tagWeight = 0.3 / Double(tags?.count ?? 1)
+
+        let properties = [
             FuseProperty(name: title ?? "", weight: 0.2),
             FuseProperty(name: description ?? "", weight: 0.2),
             FuseProperty(name: notes ?? "", weight: 0.2),
             FuseProperty(name: value, weight: 0.1),
-            FuseProperty(name: tags?.joined(separator: " ") ?? "", weight: 0.3),
         ]
+        let tagProperties = tags?.map { FuseProperty(name: $0, weight: tagWeight) }
+        return properties + (tagProperties ?? [])
     }
 }
 
